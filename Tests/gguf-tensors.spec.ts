1878e0503e730af89038c47fc2e553b75a31e741
euriklis <velislav.karastoychev@conceptdigital.bg>
2024-09-26 18:00:43 +0300

    Cleaned tests for the tensors.

--- Tests/gguf-tensors.spec.ts
+++ Tests/gguf-tensors.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from "bun:test";
 import { gguf } from "@huggingface/gguf";
 import { GGUF } from "../src";
 import validator from "@euriklis/validator-ts";
+import { halfFloat2Float } from "../src/Models";
 const url = "../../LLM/BgGPT-7B-Instruct-v0.2.F16.gguf";
 
 const model = new GGUF(url);
@@ -11,6 +12,8 @@ const ggufData = await gguf(url, {
 });
 await model.load();
 const tensorInfos = model.tensorInfos;
+const tensor = model.readTensorByIndex(0);
+if (!tensor) console.log(model.logs);
 const answer = new validator(ggufData.tensorInfos)
   .isSame(tensorInfos)
   .describe("The GGUF tensorInfos getter/setter method has to:")
