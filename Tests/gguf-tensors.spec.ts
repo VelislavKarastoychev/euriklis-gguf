fd763eb57cab9703f7c310e14aa1346506b3ea41
euriklis <velislav.karastoychev@conceptdigital.bg>
2024-09-26 13:42:19 +0300

    Implemented test for the stensorInfos getter/setter method.

---
+++ Tests/gguf-tensors.spec.ts
@@ -0,0 +1,23 @@
+"use strict";
+import { test, expect } from "bun:test";
+import { gguf } from "@huggingface/gguf";
+import { GGUF } from "../src";
+import validator from "@euriklis/validator-ts";
+const url = "../../LLM/BgGPT-7B-Instruct-v0.2.F16.gguf";
+
+const model = new GGUF(url);
+const ggufData = await gguf(url, {
+  allowLocalFile: true,
+});
+await model.load();
+const tensorInfos = model.tensorInfos;
+const answer = new validator(ggufData.tensorInfos)
+  .isSame(tensorInfos)
+  .describe("The GGUF tensorInfos getter/setter method has to:")
+  .test({ title: true, success: "green", error: "red" })
+  .describe(
+    "1. return an array of object items which contain information for each tensor.",
+  )
+  .test().answer;
+
+test("GGUF tensorInfos", () => expect(answer).toBe(true));
