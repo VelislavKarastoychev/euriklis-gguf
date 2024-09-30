a4f11ac2d2bf3faffa8209cc9b889a9dced06c10
euriklis <velislav.karastoychev@conceptdigital.bg>
2024-09-30 17:09:52 +0300

    Added new utiloity functions.

--- src/Models/index.ts
+++ src/Models/index.ts
@@ -9,3 +9,6 @@ export { alignOffset } from "./alignOffset";
 export { halfFloat2Float } from "./halfFloat2Float";
 export { float2HalfFloat } from "./float2HalfFloat";
 export { readBytesFromFile } from "./readBytesFromFile";
+export { calculateTensorDataEnd } from "./calculateTensorDataEnd";
+export { convertDataToJSType } from "./convertDataToJSType";
+
