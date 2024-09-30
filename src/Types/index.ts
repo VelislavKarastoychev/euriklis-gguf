9b31ac89d166ad0a3392e0086d5cb0ca90f4f747
euriklis <velislav.karastoychev@conceptdigital.bg>
2024-09-30 17:10:43 +0300

    Added types for typed array (constructor) and TensorInfosElementType.

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -21,10 +21,29 @@ export type TensorType =
 
 export type GGUFLogsType = { date: string; message: string };
 
-export type TensorInfosType = {
+export type TensorInfosElementType = {
   name: string;
   n_dims: number;
   shape: bigint[];
   dtype: Integer;
   offset: bigint;
-}[];
+};
+export type TypedArrayConstructorType =
+  | Uint8ArrayConstructor
+  | Int8ArrayConstructor
+  | Uint16ArrayConstructor
+  | Int16ArrayConstructor
+  | Uint32ArrayConstructor
+  | Int32ArrayConstructor
+  | Float32ArrayConstructor
+  | Float64ArrayConstructor;
+export type TensorInfosType = TensorInfosElementType[];
+export type TypedArray =
+  | Uint8Array
+  | Int8Array
+  | Uint16Array
+  | Int16Array
+  | Uint32Array
+  | Int32Array
+  | Float32Array
+  | Float64Array;
